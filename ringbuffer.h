2a4cdcf0469f990ae471ebe8e2fd9907a7f48d35
Morok1 <zhenyasmagin@yandex.ru>
2015-05-30 11:33:18 +0300

    Update ringbuffer.h

--- ringbuffer.h
+++ ringbuffer.h
@@ -6,6 +6,14 @@ typedef struct buffer{
 	int size;
 	int count;
 	int start;
+	/* different types of buffer: 
+    int *element;   - array of integers
+    char *element;  - array of characters 
+    void *element;  - array of void type (could cast to int, char, etc)
+    char **element;  - array of char pointers (array of strings)
+    void **element; - array of void pointers
+    Choosing array of void pointers since it's the most flexible */
+	
 	void **element;
 } buffer_t;
  void create(buffer_t *, int );
