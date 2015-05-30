8656d629f32615c4f19ffeabed7e2decd2026e98
Morok1 <zhenyasmagin@yandex.ru>
2015-05-30 12:04:42 +0300

    Update test.c
    Создаем буфер заполняем его.

--- test.c
+++ test.c
@@ -1,3 +1,8 @@
 #include<stdlib.h>
 #include"ringbuffer.h"
 #include"declaration.h"
+
+buffer_t buffer;
+    create(&buffer, 5);
+    char *data[] = {"first" , "second", "third", "fourth" , "fifth", "sixth"};
+    int i;
