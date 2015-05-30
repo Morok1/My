cd3a28632ac5a07c02009e7053d7756a1a7d9adc
Morok1 <zhenyasmagin@yandex.ru>
2015-05-30 12:05:21 +0300

    Update test.c
    выводим содержимое на  экран.

--- test.c
+++ test.c
@@ -6,3 +6,12 @@ buffer_t buffer;
     create(&buffer, 5);
     char *data[] = {"first" , "second", "third", "fourth" , "fifth", "sixth"};
     int i;
+for (i = 0; i < 6; i++) {
+        printf("push: %s\n", data[i]);
+        push(&buffer, data[i]);
+    }
+    printf("\n");
+    for (i = 0; i < 6; i++) {
+      
+        printf("pop from queue: %s\n", (char *)pop(&buffer));
+    }
